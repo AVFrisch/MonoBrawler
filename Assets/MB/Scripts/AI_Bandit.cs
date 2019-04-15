d511f7d37978b3ec7506ebe1971600e5e0730894
avfrisch <ahmed.v.frisch@gmail.com>
2019-04-15 12:13:57 -0400

    Added a second character that is not controlled by the player

---
+++ Assets/MB/Scripts/AI_Bandit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Bandit : MonoBehaviour
+{
+    private Animator animator;
+
+    public float strikeInterval = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        strikeInterval -= Time.deltaTime;
+
+        if (strikeInterval < 0)
+        {
+            animator.
+        }
+
+    }
+}
